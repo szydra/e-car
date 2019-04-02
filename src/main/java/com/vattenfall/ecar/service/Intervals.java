f4b23f21f45d12980d55b7f77dd7153ae2202319
szydra <szymon.draga@gmail.com>
2019-04-02 19:49:55 +0200

    Utility intervals class added

---
+++ src/main/java/com/vattenfall/ecar/service/Intervals.java
@@ -0,0 +1,107 @@
+package com.vattenfall.ecar.service;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+import static java.time.temporal.ChronoUnit.MINUTES;
+
+/**
+ * Utility class for calculating length in minutes of the intersection of time intervals.
+ */
+class Intervals {
+
+    /**
+     * Calculates the number of minutes which {@link DateTimeInterval} and {@link TimeInterval} have in common.
+     *
+     * @param dateTimeInterval date time interval
+     * @param timeInterval     day time interval
+     * @return length of the intersection in minutes, 0 if the intervals are disjoint
+     */
+    static long intersectionInMinutes(DateTimeInterval dateTimeInterval, TimeInterval timeInterval) {
+        LocalDateTime startDateTime = dateTimeInterval.start;
+        LocalDateTime endDateTime = dateTimeInterval.end;
+        if (DAYS.between(startDateTime, endDateTime) > 0) {
+            DateTimeInterval interval = new DateTimeInterval(startDateTime.plusDays(1), endDateTime);
+            return timeInterval.length() + intersectionInMinutes(interval, timeInterval);
+        } else {
+            TimeInterval interval = new TimeInterval(startDateTime.toLocalTime(), endDateTime.toLocalTime());
+            return intersectionInMinutes(interval, timeInterval);
+        }
+    }
+
+    /**
+     * Calculates the number of minutes which two {@link TimeInterval} objects have in common.
+     * This function is symmetric.
+     *
+     * @param interval1 day time interval
+     * @param interval2 day time interval
+     * @return length of the intersection in minutes, 0 if the intervals are disjoint
+     */
+    static long intersectionInMinutes(TimeInterval interval1, TimeInterval interval2) {
+        LocalTime start1 = interval1.start;
+        LocalTime end1 = interval1.end;
+        LocalTime start2 = interval2.start;
+        LocalTime end2 = interval2.end;
+        // In case where two periods contain midnight, intersection is disconnected
+        if (start1.isAfter(end1) && start2.isAfter(end2)) {
+            return MINUTES.between(LocalTime.of(0, 0), earlier(end1, end2))
+                    + DAYS.getDuration().toMinutes() - MINUTES.between(LocalTime.of(0, 0), later(start1, end2));
+        }
+        // We can assume that interval1 doesn't contain midnight
+        if (start1.isAfter(end1)) {
+            return intersectionInMinutes(interval2, interval1);
+        }
+        if (start2.isBefore(end2)) {
+            return Math.max(0, MINUTES.between(later(start1, start2), earlier(end1, end2)));
+        } else {
+            return Math.max(0, MINUTES.between(start1, earlier(end1, end2)))
+                    + Math.max(0, MINUTES.between(later(start1, start2), end1));
+        }
+    }
+
+    private static LocalTime earlier(LocalTime time1, LocalTime time2) {
+        return time1.isBefore(time2) ? time1 : time2;
+    }
+
+    private static LocalTime later(LocalTime time1, LocalTime time2) {
+        return time1.isBefore(time2) ? time2 : time1;
+    }
+
+    /**
+     * Represents date time interval, i.e. interval between two {@link LocalDateTime} objects.
+     */
+    static class DateTimeInterval {
+        private final LocalDateTime start;
+        private final LocalDateTime end;
+
+        DateTimeInterval(LocalDateTime start, LocalDateTime end) {
+            if (start.isAfter(end)) {
+                throw new IllegalArgumentException("Start has to be before end");
+            }
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    /**
+     * Represents day time interval, i.e. interval between two {@link LocalTime} objects.
+     */
+    static class TimeInterval {
+        private final LocalTime start;
+        private final LocalTime end;
+
+        TimeInterval(LocalTime start, LocalTime end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        private long length() {
+            if (start.isBefore(end)) {
+                return MINUTES.between(start, end);
+            } else {
+                return DAYS.getDuration().toMinutes() - MINUTES.between(end, start);
+            }
+        }
+    }
+}
---
+++ src/test/java/com/vattenfall/ecar/service/IntervalsTest.java
@@ -0,0 +1,96 @@
+package com.vattenfall.ecar.service;
+
+import com.vattenfall.ecar.service.Intervals.DateTimeInterval;
+import com.vattenfall.ecar.service.Intervals.TimeInterval;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+
+public class IntervalsTest {
+
+    @Test
+    public void shouldCalculateIntersectionOfNestedIntervals() {
+        LocalDate date = LocalDate.of(2019, Month.APRIL, 1);
+        LocalTime start = LocalTime.of(10, 30);
+        LocalTime end = LocalTime.of(20, 30);
+        DateTimeInterval dateTimeInterval = new DateTimeInterval(LocalDateTime.of(date, start), LocalDateTime.of(date, end));
+
+        TimeInterval timeInterval = new TimeInterval(LocalTime.of(12, 15), LocalTime.of(16, 0));
+
+        assertThat(Intervals.intersectionInMinutes(dateTimeInterval, timeInterval)).isEqualTo(45 + 3 * 60);
+    }
+
+    @Test
+    public void shouldCalculateIntersectionOfNonNestedIntervals() {
+        LocalDate date = LocalDate.of(2019, Month.APRIL, 1);
+        LocalTime start = LocalTime.of(10, 30);
+        LocalTime end = LocalTime.of(20, 30);
+        DateTimeInterval dateTimeInterval = new DateTimeInterval(LocalDateTime.of(date, start), LocalDateTime.of(date, end));
+
+        TimeInterval timeInterval = new TimeInterval(LocalTime.of(12, 15), LocalTime.of(23, 0));
+
+        assertThat(Intervals.intersectionInMinutes(dateTimeInterval, timeInterval)).isEqualTo(45 + 7 * 60 + 30);
+    }
+
+    @Test
+    public void shouldCalculateIntersectionOfLongerThanOneDayIntervalWithIntervalNonContainingMidnight() {
+        LocalDate startDate = LocalDate.of(2019, Month.APRIL, 1);
+        LocalDate endDate = LocalDate.of(2019, Month.APRIL, 5);
+        LocalTime startTime = LocalTime.of(10, 30);
+        LocalTime endTime = LocalTime.of(20, 30);
+        DateTimeInterval dateTimeInterval = new DateTimeInterval(LocalDateTime.of(startDate, startTime), LocalDateTime.of(endDate, endTime));
+
+        TimeInterval timeInterval = new TimeInterval(LocalTime.of(12, 15), LocalTime.of(13, 0));
+
+        assertThat(Intervals.intersectionInMinutes(dateTimeInterval, timeInterval)).isEqualTo(5 * 45);
+    }
+
+    @Test
+    public void shouldCalculateIntersectionOfLongerThanOneDayWithIntervalWithIntervalContainingMidnight() {
+        LocalDate startDate = LocalDate.of(2019, Month.APRIL, 1);
+        LocalDate endDate = LocalDate.of(2019, Month.APRIL, 5);
+        LocalTime startTime = LocalTime.of(10, 30);
+        LocalTime endTime = LocalTime.of(20, 30);
+        DateTimeInterval dateTimeInterval = new DateTimeInterval(LocalDateTime.of(startDate, startTime), LocalDateTime.of(endDate, endTime));
+
+        TimeInterval timeInterval = new TimeInterval(LocalTime.of(22, 0), LocalTime.of(1, 20));
+
+        assertThat(Intervals.intersectionInMinutes(dateTimeInterval, timeInterval)).isEqualTo(4 * 200);
+    }
+
+    @Test
+    public void shouldCalculateIntersectionOfTwoIntervalsContainingMidnight() {
+        TimeInterval interval1 = new TimeInterval(LocalTime.of(22, 0), LocalTime.of(3, 15));
+        TimeInterval interval2 = new TimeInterval(LocalTime.of(20, 30), LocalTime.of(1, 15));
+
+        assertThat(Intervals.intersectionInMinutes(interval1, interval2)).isEqualTo(3 * 60 + 15);
+    }
+
+    @Test
+    public void shouldCalculateIntersectionWhenFirstIntervalContainsMidnight() {
+        TimeInterval interval1 = new TimeInterval(LocalTime.of(22, 0), LocalTime.of(3, 15));
+        TimeInterval interval2 = new TimeInterval(LocalTime.of(0, 30), LocalTime.of(1, 15));
+
+        assertThat(Intervals.intersectionInMinutes(interval1, interval2)).isEqualTo(45);
+    }
+
+    @Test
+    public void shouldThrowIllegalArgumentExceptionWhenCreatingInvalidDateTimeInterval() {
+        LocalDate date = LocalDate.of(2019, Month.APRIL, 1);
+        LocalTime startTime = LocalTime.of(13, 35);
+        LocalTime endTime = LocalTime.of(13, 34);
+        LocalDateTime start = LocalDateTime.of(date, startTime);
+        LocalDateTime end = LocalDateTime.of(date, endTime);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new DateTimeInterval(start, end))
+                .withMessage("Start has to be before end");
+    }
+}
