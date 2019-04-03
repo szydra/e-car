1f765c5d1faccfd414925214728ea5498f42ae83
szydra <szymon.draga@gmail.com>
2019-04-03 18:41:00 +0200

    Return collection instead of iterable in price repository

--- src/main/java/com/vattenfall/ecar/repository/PriceRepository.java
+++ src/main/java/com/vattenfall/ecar/repository/PriceRepository.java
@@ -3,5 +3,10 @@ package com.vattenfall.ecar.repository;
 import com.vattenfall.ecar.model.Price;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collection;
+
 public interface PriceRepository extends CrudRepository<Price, Integer> {
+
+    Collection<Price> findAll();
+
 }
