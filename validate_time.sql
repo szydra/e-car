f2c48d93dc6f029129cf854a6d2e2a65b367ed2d
szydra <szymon.draga@gmail.com>
2019-04-03 21:33:27 +0200

    Improve constraint definition

--- validate_time.sql
+++ validate_time.sql
@@ -5,11 +5,11 @@ import java.sql.SQLException;
 import java.sql.Statement;
 @CODE
     boolean isValid(final Connection connection, Integer id, String start, String end) throws SQLException {
-        if (start == null || end == null) {
-            return true;
-        }
+        Statement statement = connection.createStatement();
         String sql;
-        if (start.compareTo(end) > 0) {
+        if (start == null || end == null) {
+            sql = "select count(*) as total from price where rate_type='BASIC' and id!=" + id;
+        } else if (start.compareTo(end) > 0) {
             sql = String.format("select count(*) as total from price where " +
                     "(start_hour<end_hour and ('%s'<end_hour or start_hour<'%s') and id!=%s) " +
                     "or (end_hour<start_hour and id!=%s);", start, end, id, id);
@@ -18,7 +18,6 @@ import java.sql.Statement;
                     "(start_hour<end_hour and '%s'<end_hour and start_hour<'%s' and id!=%s) " +
                     "or (end_hour<start_hour and ('%s'<end_hour or start_hour<'%s') and id!=%s);", start, end, id, start, end, id);
         }
-        Statement statement = connection.createStatement();
         ResultSet set = statement.executeQuery(sql);
         set.next();
         return set.getInt("total") == 0;
